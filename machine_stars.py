19aa87e98d3822b5e6e505b7c99d6ee1f4a2e01e
Brennen Bearnes <bbearnes@gmail.com>
2015-03-13 05:36:31 +0000

    shrinking text, better stars

--- machine_stars.py
+++ machine_stars.py
@@ -2,6 +2,7 @@
 
 import sys
 import pygame
+import random
 
 pygame.init()
 
@@ -11,12 +12,9 @@ machine_rect = machine_img.get_rect()
 machine_rect.left = 10
 machine_rect.top = 120
 
-# Set up the picture of our little machine:
-star_img = pygame.image.load('star.png')
-star_img = pygame.transform.smoothscale(star_img, (10, 10))
-star_rect = star_img.get_rect()
+# Set up the picture of a star:
+original_star_img = pygame.image.load('star.png')
 
-font = pygame.font.Font(None, 20) 
 text_color = pygame.Color(255, 255, 255)
 bg_color = pygame.Color(0, 0, 0)
 
@@ -26,7 +24,9 @@ screen = pygame.display.set_mode([320, 240])
 stars_length = 0
 offset = 0
 
+# Start building a list of things to display from stdin:
 display_lines = [sys.stdin.readline()]
+
 while len(display_lines) > 0:
     screen.blit(machine_img, machine_rect)
 
@@ -34,28 +34,51 @@ while len(display_lines) > 0:
     screen.fill(bg_color, [0, 0, 320, 120])
     screen.fill(bg_color, [machine_rect.right, machine_rect.top, 320, 240])
 
+    # Get the next available line from stdin:
     line = sys.stdin.readline()
+
     if line:
         display_lines.insert(0, line)
+
+    # If there're more than 6 lines to display, or we're not getting
+    # any more input, pop the last line off the list and turn it into
+    # a number of stars to show:
     if (len(display_lines) > 6) or (not line):
         stars_length = len(display_lines.pop())
+
+    # If there's no more input, start offsetting display from the top
+    # of the screen so it seems to fall downwards:
     if not line:
         offset = offset + 20
 
-    # Display the input falling into the machine's input:
+    # Display the most recent lines of stdin falling into the machine,
+    # in a font that gets smaller as it falls:
+    font_size = 22
     y = 0 + offset
-    for render_line in display_lines:
-        input_text_surface = font.render(render_line.rstrip(), True, text_color)
-        input_text_rect = input_text_surface.get_rect(left = 24, top = y)
+    for render_me in display_lines:
+        font_size = font_size - 2
+        font = pygame.font.Font(None, font_size) 
+        input_text_surface = font.render(render_me.rstrip(), True, text_color)
+        input_text_rect = input_text_surface.get_rect(center = (64, y))
         screen.blit(input_text_surface, input_text_rect)
         y = y + 20
 
-    if stars_length > 0:
-        for star_i in range(0, stars_length):
-            star_rect.left = machine_rect.right + 8 + (star_i * star_rect.width)
-            star_rect.top = 190
-            screen.blit(star_img, star_rect)
-
     pygame.display.update()
 
-    pygame.time.wait(400);
+    # Display stars leaving machine's output:
+    if stars_length > 0:
+        star_x = machine_rect.right
+        for i in range(0, stars_length):
+            star_w = random.randrange(10, 28)
+            star_h = random.randrange(10, 28)
+            star_x = star_x + star_w
+            star_img = pygame.transform.smoothscale(original_star_img, (star_w, star_h))
+            star_rect = star_img.get_rect()
+            pygame.display.update()
+            pygame.time.wait(15)
+            star_rect.left = star_x
+            star_rect.top = 185 + random.randrange(-5, 5)
+            screen.blit(star_img, star_rect)
+
+    # Chill out for for 400 milliseconds:
+    pygame.time.wait(200)
