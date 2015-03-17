20cf4d1713ec715c96e8193e0afa627daa71ba77
Brennen Bearnes <bbearnes@gmail.com>
2015-03-17 15:54:22 +0000

    raspipe.py: toggle-able star display

--- raspipe.py
+++ raspipe.py
@@ -1,81 +1,96 @@
-#!/usr/bin/env python3
-# encoding: utf-8
+#!/usr/bin/env python
 
-import math
-import os
-import pygame
-import random
-import re
-import sys
 import getopt
+import random
+import sys
+import time
+
+import pygame
 
 class RasPipe:
     size = width, height = 320, 240
-    # size = width, height = 480, 320
 
-    font_size = 48
     delay = 70
-    input_buffer_size = 100
-    input_lines = []
     display_lines = 7
+    font_size = 26
+    input_lines = []
+    tick = 0
+    stars = False
 
     def __init__(self, infile):
+        """Create a RasPipe object for a given input file."""
         self.infile = infile
+
         pygame.init()
-        exit
         self.screen = pygame.display.set_mode(self.size)
         self.setfont(self.font_size)
 
-        self.bgcolor = pygame.Color(0, 0, 0)
-        self.fgcolor = pygame.Color(255, 255, 255)
+        self.fgcolor = pygame.Color(0, 0, 0)
+        self.bgcolor = pygame.Color(255, 255, 255)
 
         # A little bit of sound.
         pygame.mixer.init()
         self.click = pygame.mixer.Sound('./tick.wav')
 
+        # Set up the picture of a star:
+        self.orig_star_img = pygame.image.load('star.png').convert_alpha()
+
     def setfont(self, size):
+        """Set a display font of a given size."""
         self.font = pygame.font.Font(None, self.font_size) 
 
+    def toggle_stars(self):
+        self.stars = not self.stars
+
     def run(self):
-        tick = 0
-
-        self.click.play()
-
+        """Process standard input."""
         line = self.infile.readline()
         while line:
             for event in pygame.event.get():
-              if event.type == pygame.QUIT:
-                sys.exit()
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.toggle_stars()                 
 
-            tick = tick + 1
-
+            self.tick += 1
             self.scale_display()
-
             self.screen.fill(self.bgcolor)
 
-            # Get last display_lines of input...
+            # Get last display_lines of input and scroll them up display:
             to_render = self.input_lines[-self.display_lines:]
-
-            # ...and scroll them up the display:
             y = 0
             for render_line in to_render:
-                y += self.font_size
-                text_surface = self.font.render(render_line.rstrip(), True, self.fgcolor)
-                # TODO: allow centering text
-                # rect = text_surface.get_rect(center=((self.width / 2), y))
-                rect = text_surface.get_rect(left=2, top=y)
-                self.screen.blit(text_surface, rect)
-            
-            # A progress bar of sorts
-            progress = (self.width / 100) * (len(self.input_lines) / 10)
-            self.screen.fill(self.fgcolor, [0, 0, progress, self.font_size])
+                render_line = render_line.rstrip()
 
-            if tick % self.display_lines == 0:
+                if self.stars:
+                    star_x = 0
+                    stars_length = len(render_line)
+                    for i in range(0, stars_length):
+                        star_w = random.randrange(8, 40)
+                        star_h = random.randrange(8, 40)
+                        star_img = pygame.transform.smoothscale(self.orig_star_img, (star_w, star_h))
+                        star_rect = star_img.get_rect()
+                        star_rect.left = star_x
+                        star_rect.top = y + random.randrange(-4, 4)
+                        self.screen.blit(star_img, star_rect)
+                        star_x += star_w
+                        if star_x > 320:
+                            break
+                        pygame.display.update()
+
+                else:
+                    text_surface = self.font.render(render_line, True, self.fgcolor)
+                    r = text_surface.get_rect(left=2, top=y)
+                    self.screen.blit(text_surface, r)
+
+                    for pixel_x in range(0, len(render_line.rstrip())):
+                        if render_line[pixel_x] != ' ':
+                            pygame.draw.line(self.screen, self.fgcolor, [pixel_x, r.bottom], [pixel_x, r.bottom], 1)
+
+                y += self.font_size
+
+            if self.tick % self.display_lines == 0:
                 self.click.play()
-                # self.bgcolor.r = random.randrange(0, 255)
-                # self.bgcolor.g = random.randrange(0, 255)
-                # self.bgcolor.b = random.randrange(0, 255)
-                # self.bgcolor.a = random.randrange(0, 255)
             
             # Actually display the display:
             pygame.display.flip()
@@ -86,11 +101,12 @@ class RasPipe:
             line = self.infile.readline()
 
     def scale_display(self):
-       """Set the current font size and delay based on amount of input"""
+       """Set the current font size and delay based on amount of input."""
        original_font_size = self.font_size
 
        # How big should our font be, and how fast should text scroll?
        if len(self.input_lines) > 150:
+           self.input_lines.pop(0)
            self.font_size = 18
            self.delay = 5
        elif len(self.input_lines) > 60:
@@ -104,14 +120,19 @@ class RasPipe:
            self.setfont(self.font_size)
    
        # How many lines of text to display?
-       self.display_lines = int(self.size[1] / self.font_size) - 1
+       self.display_lines = int(self.size[1] / self.font_size)
 
+# Handle running this file as a standalone script.
 if __name__ == '__main__':
     rp = RasPipe(sys.stdin)
 
-    opts, args = getopt.getopt(sys.argv[1:], 'f:')
+    opts, args = getopt.getopt(sys.argv[1:], 'x:y:s')
     for opt, arg in opts:
-        if opt == '-f':
-            rp.setfont(int(arg))
+        if opt == '-x':
+            rp.size[0] = (int(arg))
+        if opt == '-y':
+            rp.size[1] = (int(arg))
+        if opt == '-s':
+            rp.toggle_stars()
 
     rp.run()
